3119d75dd2b39914d29e918ffd3a971475748cae
Md Shad Khan <shadkhanff>
2022-03-01 19:15:55 +0530

    greeting system added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let curDate = new Date();
+  curDate = curDate.getHours();
+  let greeting = '';
+  const cssStyle = {};
+
+  if(curDate >=1 && curDate <12){
+    greeting = 'Good Morning';
+    cssStyle.color = 'Green';
+  }
+  else if(curDate >=12 && curDate <19){
+    greeting = 'Good Afternoon';
+    cssStyle.color = 'Orange';
+  }
+  else{
+    greeting = 'Good Night';
+    cssStyle.color = 'Grey';    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+     <h1>Hello Shad Khan, <span style={cssStyle}> { greeting } </span></h1>
+    </div>
     </div>
   );
 }
